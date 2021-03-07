3647db5578e82d98ec6580ee784b4b0d5b742464
ItzWillz <william.x.mcdonald@eagles.oc.edu>
2021-03-06 23:05:37 -0600

    Added Character Movement Script
    Added character movement script

---
+++ Assets/characterMovement.cs
@@ -0,0 +1,41 @@
+/* Script by William McDonald and Johnathon Cortez, March 6th 9:40pm.
+ * Movement script, for moving an asset with the  'WASD' keys, can be future modified for controller input.*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+
+    public float speed = 10.4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("W") || Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("S") || Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("D") || Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("A") || Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+
+    }
+}
